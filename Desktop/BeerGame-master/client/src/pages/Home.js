c20b17e50bae0727a4860c2ab338aad95e833612
Mhammed El Mrabet <98460610+mhammed21@users.noreply.github.com>
2023-05-04 11:25:28 +0200

    Update Home.js

--- Desktop/BeerGame-master/client/src/pages/Home.js
+++ Desktop/BeerGame-master/client/src/pages/Home.js
@@ -7,24 +7,22 @@ import { Link } from "react-router-dom";
 class Home extends React.Component {
   render() {
     return (
-      <div className={"home"}>
-        <h2>Bienvenue sur la page d'accueil du Beergame ! Un projet commun de la Welfen Academy et de la LeibnizFH.</h2>
-        <div className={"wrapper_logos"}>
-          <a href="https://leibniz-fh.de/" id="logo">
-            <div className={"logo leibniz"} />
-          </a>
-          <div className={"logo Beergame"} />
-          <a href="https://www.welfenakademie.de/" id="logo">
-            <div className={"logo welfen"} />
-          </a>
+      <>
+        <div className={"container"}>
+          <div className={"pic"}>
+            {/*<img src="../res/images/beerGameProcess.png" alt="beer game" />*/}
+          </div>
         </div>
-        <h4>Le but du jeu est d'éviter les goulots d'étranglement d'approvisionnement et de pouvoir servir les commandes au sein de la chaîne d'approvisionnement à tout moment, malgré les fluctuations de la demande.</h4>
-        <div className={"wrapper_button"}>
-          <Link to={"/game/create"}>
-            <Button linkTo={"/game/create"}>démarrer jeu</Button>
-          </Link>
+        <div className={"home"}>
+          
+          <div className={"wrapper_button"}>
+            <Link to={"/game/create"}>
+              <Button linkTo={"/game/create"}>Démarrer jeu</Button>
+            </Link>
+          </div>
         </div>
-      </div>
+      </>
+      
     )
   }
 }
