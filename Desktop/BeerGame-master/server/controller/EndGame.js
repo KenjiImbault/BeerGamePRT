6bdb17f0736680d59586798b88c3ad6e2197a82d
Kenji Imbault <kenjiimbault@gmail.com>
2023-05-06 12:42:10 +0200

    Update EndGame.js

--- Desktop/BeerGame-master/server/controller/EndGame.js
+++ Desktop/BeerGame-master/server/controller/EndGame.js
@@ -1,64 +1,54 @@
 import mongoose from "mongoose"
 
 import DBGame from "../model/DBGame.js";
-import SocketError from "../model/SocketError.js";
-import SocketSuccess from "../model/SocketSuccess.js";
 import exceljs from "exceljs";
 
-const socket = io.connect("http://localhost:3001")
-  //const socket = io.connect("https://api-beergame.usb-sys.de")
-  useEffect(() => {
+//const socket = io.connect("https://api-beergame.usb-sys.de")
 
+export default function EndGame(io, socket, data) {
+  const GameData = mongoose.model("DBGame", DBGame)
+  const Current = new GameData(data)
+      
+  // const exceljs = require('exceljs');
+  const workbook = new exceljs.Workbook();
+  const worksheetClient = workbook.addWorksheet('Client');
+  const worksheetRetailer = workbook.addWorksheet('Retailer');
+  const worksheetWholesaler = workbook.addWorksheet('Wholesaler');
+  const worksheetDistributor = workbook.addWorksheet('Distributor');
+  const worksheetProducer = workbook.addWorksheet('Producer');
 
-socket.on("endGame", () => {
+  console.log("EndGame.js")
 
-    function EndGame(io, socket, data) {
-    
-        const GameData = mongoose.model("DBGame", DBGame)
-        const Current = new GameData(data)
-      
-        // const exceljs = require('exceljs');
-       const workbook = new exceljs.Workbook();
-       const worksheetClient = workbook.addWorksheet('Client');
-       const worksheetRetailer = workbook.addWorksheet('Retailer');
-       const worksheetWholesaler = workbook.addWorksheet('Wholesaler');
-       const worksheetDistributor = workbook.addWorksheet('Distributor');
-       const worksheetProducer = workbook.addWorksheet('Producer');
-      
-       GameData.find({}, (err, data) => {
-         if (err) throw err;
-       
-         // Ajouter l'en-tête du tableau
-         worksheetClient.addRow(['test']);
-         worksheetRetailer.addRow(['stock','order','delay','next1Week','next2Week']);
-         worksheetWholesaler.addRow(['stock','order','delay','next1Week','next2Week']);
-         worksheetDistributor.addRow(['stock','order','delay','next1Week','next2Week']);
-         worksheetProducer.addRow(['stock','order','delay','next1Week','next2Week']);
-      
-      
-      
-         let numberOfRounds = 0
-         let lastGame = 0
-      
-         lastGame = data.length
+      GameData.find({}, (err, data) => {
+
         
+        // Ajouter l'en-tête du tableau
+        worksheetClient.addRow(['test']);
+        worksheetRetailer.addRow(['stock','order','delay','next1Week','next2Week']);
+        worksheetWholesaler.addRow(['stock','order','delay','next1Week','next2Week']);
+        worksheetDistributor.addRow(['stock','order','delay','next1Week','next2Week']);
+        worksheetProducer.addRow(['stock','order','delay','next1Week','next2Week']);
+      
+        let numberOfRounds = 0
+        let lastGame = 0
+      
+        lastGame = data.length
+        let roundDataLastGame = data[lastGame-1].roundData
          
-         numberOfRounds = data[0].roundData.producer.length
-         for (let i = 0;i<numberOfRounds;i++){
-          worksheetRetailer.addRow([data[lastGame].roundData.retailer[i].stock,data[lastGame].roundData.retailer[i].order,data[lastGame].roundData.retailer[i].delay,0,0])
-          worksheetWholesaler.addRow([data[lastGame].roundData.wholesaler[i].stock,data[lastGame].roundData.wholesaler[i].order,data[lastGame].roundData.wholesaler[i].delay,0,0])
-          worksheetDistributor.addRow([data[lastGame].roundData.distributor[i].stock,data[lastGame].roundData.distributor[i].order,data[lastGame].roundData.distributor[i].delay,0,0])
-          worksheetProducer.addRow([data[lastGame].roundData.producer[i].stock,data[lastGame].roundData.producer[i].order,data[lastGame].roundData.producer[i].delay,0,0])
-         }
+        numberOfRounds = data[lastGame-1].roundData.producer.length
+        for (let i = 0;i<numberOfRounds;i++){
+          
+          worksheetRetailer.addRow([roundDataLastGame.retailer[i].stock,roundDataLastGame.retailer[i].order,roundDataLastGame.retailer[i].delay,0,0])
+          worksheetWholesaler.addRow([roundDataLastGame.wholesaler[i].stock,roundDataLastGame.wholesaler[i].order,roundDataLastGame.wholesaler[i].delay,0,0])
+          worksheetDistributor.addRow([roundDataLastGame.distributor[i].stock,roundDataLastGame.distributor[i].order,roundDataLastGame.distributor[i].delay,0,0])
+          worksheetProducer.addRow([roundDataLastGame.producer[i].stock,roundDataLastGame.producer[i].order,roundDataLastGame.producer[i].delay,0,0])
+        }
       
-       
-         // Enregistrer le fichier Excel
-         workbook.xlsx.writeFile('export.xlsx')
-           .then(() => console.log('Fichier Excel créé avec succès'))
-           .catch((err) => console.error('Erreur lors de la création du fichier Excel', err));
-       });
-      
-        
-      }
+        // Enregistrer le fichier Excel
+        workbook.xlsx.writeFile('export.xlsx')
+        .then(() => console.log('Fichier Excel créé avec succès'))
+        .catch((err) => console.error('Erreur lors de la création du fichier Excel', err));
+      });      
+}
 
-}) })
+ 
