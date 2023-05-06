e1665d3716509a58ef9a0f1bd7b9fcfbbed6680b
Kenji Imbault <kenjiimbault@gmail.com>
2023-05-06 12:41:41 +0200

    Update index.js

--- Desktop/BeerGame-master/server/index.js
+++ Desktop/BeerGame-master/server/index.js
@@ -9,6 +9,7 @@ import JoinGame from "./controller/JoinGame.js";
 import CreateGame from "./controller/CreateGame.js";
 import UpdateGame from "./controller/UpdateGame.js";
 import LeaveGame from "./controller/LeaveGame.js";
+import EndGame from "./controller/EndGame.js";
 
 const io = new Server({
   cors: {
@@ -23,6 +24,7 @@ mongoose.connect(process.env.MONGOOSE_CONNECTIONSTRING)
       socket.on("game_create", (data) => CreateGame(io, socket, data))
       socket.on("game_update", (data) => UpdateGame(io, socket, data))
       socket.on("disconnect", () => LeaveGame(io, socket))
+      socket.on("endGame", (data) =>EndGame(io,socket,data))
     });
     console.log("serveur sur le port " + process.env.SERVER_PORT + " commencé. Connexion à la base de données établie.");
   })
